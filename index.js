49840222be00ffe338ffcd0b9343fc4e1f7e21fb
Mathias Buus <mathiasbuus@gmail.com>
2017-03-27 20:22:58 +0200

    support versioning

--- index.js
+++ index.js
@@ -9,57 +9,6 @@ module.exports = serve
 function serve (archive) {
   return onrequest
 
-  function onfile (name, opts, req, res) {
-    archive.stat(name, function (err, st) {
-      if (err) return onerror(res, 404, err)
-
-      var r = req.headers.range && range(st.size, req.headers.range)[0]
-      res.setHeader('Accept-Ranges', 'bytes')
-      res.setHeader('Content-Type', mime.lookup(name))
-
-      if (r) {
-        res.statusCode = 206
-        res.setHeader('Content-Range', 'bytes ' + r.start + '-' + r.end + '/' + st.size)
-        res.setHeader('Content-Length', r.end - r.start + 1)
-      } else {
-        res.setHeader('Content-Length', st.size)
-      }
-
-      if (req.method === 'HEAD') return res.end()
-      pump(archive.createReadStream(name, r), res)
-    })
-  }
-
-  function ondirectory (name, opts, req, res) {
-    archive.stat(name + 'index.html', function (err) {
-      if (err) return ondirectoryindex(name, opts, req, res)
-      onfile(name + 'index.html', opts, req, res)
-    })
-  }
-
-  function ondirectoryindex (name, opts, req, res) {
-    list(archive, name, function (err, entries) {
-      if (err) entries = []
-
-      var wait = archive.metadata ? archive.metadata.length + 1 : 0
-      var script = `
-        var xhr = new XMLHttpRequest()
-        xhr.open("GET", "${name}?wait=${wait}", true)
-        xhr.onload = function () {
-          document.open()
-          document.write(xhr.responseText)
-          document.close()
-        }
-        xhr.send(null)
-      `
-
-      var html = toHTML({directory: name, script: script}, entries)
-      res.setHeader('Content-Type', 'text/html; charset=utf-8')
-      res.setHeader('Content-Length', Buffer.byteLength(html))
-      res.end(html)
-    })
-  }
-
   function onrequest (req, res) {
     var name = decodeURI(req.url.split('?')[0])
     var query = qs.parse(req.url.split('?')[1] || '')
@@ -71,10 +20,62 @@ function serve (archive) {
     waitFor(archive, wait, ready)
 
     function ready () {
-      if (name[name.length - 1] === '/') ondirectory(name, query, req, res)
-      else onfile(name, query, req, res)
+      var arch = /^\d+$/.test(query.version) ? archive.checkout(Number(query.version)) : archive
+      if (name[name.length - 1] === '/') ondirectory(arch, name, query, req, res)
+      else onfile(arch, name, query, req, res)
     }
   }
+}
+
+function onfile (archive, name, opts, req, res) {
+  archive.stat(name, function (err, st) {
+    if (err) return onerror(res, 404, err)
+
+    var r = req.headers.range && range(st.size, req.headers.range)[0]
+    res.setHeader('Accept-Ranges', 'bytes')
+    res.setHeader('Content-Type', mime.lookup(name))
+
+    if (r) {
+      res.statusCode = 206
+      res.setHeader('Content-Range', 'bytes ' + r.start + '-' + r.end + '/' + st.size)
+      res.setHeader('Content-Length', r.end - r.start + 1)
+    } else {
+      res.setHeader('Content-Length', st.size)
+    }
+
+    if (req.method === 'HEAD') return res.end()
+    pump(archive.createReadStream(name, r), res)
+  })
+}
+
+function ondirectory (archive, name, opts, req, res) {
+  archive.stat(name + 'index.html', function (err) {
+    if (err) return ondirectoryindex(archive, name, opts, req, res)
+    onfile(archive, name + 'index.html', opts, req, res)
+  })
+}
+
+function ondirectoryindex (archive, name, opts, req, res) {
+  list(archive, name, function (err, entries) {
+    if (err) entries = []
+
+    var wait = archive.metadata ? archive.metadata.length + 1 : 0
+    var script = `
+      var xhr = new XMLHttpRequest()
+      xhr.open("GET", "${name}?wait=${wait}", true)
+      xhr.onload = function () {
+        document.open()
+        document.write(xhr.responseText)
+        document.close()
+      }
+      xhr.send(null)
+    `
+
+    var html = toHTML({directory: name, script: archive._checkout ? null : script}, entries)
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    res.setHeader('Content-Length', Buffer.byteLength(html))
+    res.end(html)
+  })
 }
 
 function waitFor (archive, until, cb) { // this feels a bit hacky, TODO: make less complicated?
